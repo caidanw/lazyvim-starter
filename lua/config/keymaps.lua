3ab5cfb03ef319ac581fa333c05518a3ad17ea9d
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-05-03 13:48:31 -0700

    feat: add custom keymap to toggle macro recording functionality

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,3 +13,25 @@ keymap("x", "<leader>p", [["_dP]])
 
 -- Disable repeating last recorded register n times
 keymap("n", "Q", "<NOP>")
+
+-- Disable `q` for macro recording as default
+-- Set initial state for 'q'
+vim.g.q_record_macro = false
+
+-- Function to toggle 'q' functionality
+function _G.toggle_q_macro()
+  if vim.g.q_record_macro then
+    -- If currently set for recording macros, make 'q' do nothing
+    keymap("n", "q", "<NOP>", { noremap = true })
+    vim.g.q_record_macro = false
+    LazyVim.warn("Disabled Macro Recording", { title = "Option" })
+  else
+    -- If currently set to do nothing, make 'q' record macros
+    keymap("n", "q", "q", { noremap = true })
+    vim.g.q_record_macro = true
+    LazyVim.info("Enabled Macro Recording", { title = "Option" })
+  end
+end
+
+keymap("n", "<leader>uq", _G.toggle_q_macro, { noremap = true, silent = true, desc = "Toggle Macro Recording" })
+keymap("n", "q", "<NOP>", { noremap = true })
