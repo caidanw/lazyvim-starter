f41e5358733823832086e90705bd22ebb6b549b1
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-05-16 15:40:15 -0700

    feat: completely replace python extra with custom python plugin

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -1,27 +1,123 @@
+local lsp = "pyright"
 local ruff = "ruff"
-
--- LSP Server to use for Python.
-vim.g.lazyvim_python_lsp = "pyright"
-vim.g.lazyvim_python_ruff = ruff
 
 return {
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "ninja", "python", "rst", "toml" })
+      end
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
     opts = {
       servers = {
-        pyright = {
+        [lsp] = {
           enabled = true,
+        },
+        ---@type lspconfig.options.pyright
+        pyright = {
+          settings = {
+            ---@diagnostic disable-next-line: missing-fields
+            python = {
+              ---@diagnostic disable-next-line: missing-fields
+              analysis = {
+                autoImportCompletions = true,
+                autoSearchPaths = true,
+                diagnosticMode = "openFilesOnly",
+                typeCheckingMode = "basic",
+                useLibraryCodeForTypes = true,
+              },
+            },
+            pyright = {
+              disableLanguageServices = false,
+              disableOrganizeImports = true,
+              disableTaggedHints = false,
+            },
+          },
         },
         [ruff] = {
           enabled = true,
+          keys = {
+            {
+              "<leader>co",
+              function()
+                vim.lsp.buf.code_action({
+                  apply = true,
+                  context = {
+                    only = { "source.organizeImports" },
+                    diagnostics = {},
+                  },
+                })
+              end,
+              desc = "Organize Imports",
+            },
+          },
+        },
+      },
+      setup = {
+        [ruff] = function()
+          LazyVim.lsp.on_attach(function(client, _)
+            if client.name == ruff then
+              -- Disable hover in favor of Pyright
+              client.server_capabilities.hoverProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "nvim-neotest/neotest-python",
+    },
+    opts = {
+      adapters = {
+        ["neotest-python"] = {
+          -- Here you can specify the settings for the adapter, i.e.
+          -- runner = "pytest",
+          -- python = ".venv/bin/python",
         },
       },
     },
   },
   {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      "mfussenegger/nvim-dap-python",
+      keys = {
+        {
+          "<leader>dPt",
+          function()
+            require("dap-python").test_method()
+          end,
+          desc = "Debug Method",
+          ft = "python",
+        },
+        {
+          "<leader>dPc",
+          function()
+            require("dap-python").test_class()
+          end,
+          desc = "Debug Class",
+          ft = "python",
+        },
+      },
+      config = function()
+        local path = require("mason-registry").get_package("debugpy"):get_install_path()
+        require("dap-python").setup(path .. "/venv/bin/python")
+      end,
+    },
+  },
+  {
     "linux-cultist/venv-selector.nvim",
     cmd = "VenvSelect",
-    ft = "python",
     opts = function(_, opts)
       if LazyVim.has("nvim-dap-python") then
         opts.dap_enabled = true
@@ -36,10 +132,15 @@ return {
         },
       })
     end,
-    keys = function()
-      return {
-        { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv", ft = "python" },
-      }
+    keys = {
+      { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv", ft = "python" },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      opts.auto_brackets = opts.auto_brackets or {}
+      table.insert(opts.auto_brackets, "python")
     end,
   },
 }
