c64b3995d16af6d60d005afa9617bac587d5147f
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-05-17 14:08:37 -0700

    fix: disable troublesome plugin headlines.nvim

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,5 @@
+return {
+  -- This plugin causes issues by using a very new unicode character
+  -- See: https://github.com/neovide/neovide/issues/2536#issuecomment-2106127919
+  { "lukas-reineke/headlines.nvim", enabled = false },
+}
