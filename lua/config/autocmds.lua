2f4ae079706322292fc19f4b0ac3dd19cbb2ba4b
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-08-09 12:02:23 -0700

    feat: add jinja filetypes

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Add jinja filetype
+-- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#jinja_lsp
+vim.filetype.add({
+  extension = {
+    jinja = "jinja",
+    jinja2 = "jinja",
+    j2 = "jinja",
+  },
+})
