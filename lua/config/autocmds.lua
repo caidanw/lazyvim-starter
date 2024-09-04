88de2732bb963566dbc94c66f865f664438441f0
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-09-04 13:19:57 -0700

    feat: create a custom function to match jinja filetypes

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,17 +2,34 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Add jinja filetype
--- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#jinja_lsp
+---Determine the filetype for Jinja templates
+---@param path string: The path to the file
+---@param bufnr number: The buffer number
+---@param filename string: The name of the file
+---@return string: The determined filetype
+local function ft_jinja(path, bufnr, filename)
+  local ft, _ = vim.filetype.match({ buf = bufnr, filename = filename })
+  if not ft then
+    return "jinja"
+  end
+  return ft .. ".jinja"
+end
+
 vim.filetype.add({
-  extension = {
-    jinja = "jinja",
-    jinja2 = "jinja",
-    j2 = "jinja",
-    env = "dotenv",
+  filename = {
+    ["nginx.conf"] = "nginx",
   },
   pattern = {
     -- INFO: Match filenames like - ".env.example", ".env.local" and so on
     ["%.env%.[%w_.-]+"] = "dotenv",
+    ["(.*)%.jinja"] = ft_jinja,
+    ["(.*)%.jinja2"] = ft_jinja,
+    ["(.*)%.j2"] = ft_jinja,
+  },
+  extension = {
+    -- jinja = "jinja",
+    -- jinja2 = "jinja",
+    -- j2 = "jinja",
+    -- env = "dotenv",
   },
 })
