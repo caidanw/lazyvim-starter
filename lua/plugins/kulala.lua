846de7a72b252ec8cb0b7b1d0e5e769c7cb27d02
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-09-04 13:23:01 -0700

    feat: add plugin kulala

---
+++ lua/plugins/kulala.lua
@@ -0,0 +1,8 @@
+-- HTTP REST-Client Interface
+return {
+  -- "mistweaverco/kulala.nvim",
+  -- config = function()
+  --   -- Setup is required, even if you don't pass any options
+  --   require("kulala").setup()
+  -- end,
+}
