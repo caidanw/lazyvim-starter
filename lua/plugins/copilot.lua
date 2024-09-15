e16385cb37f76bb72c911f3c9118cfd36f18cfe1
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-09-15 16:46:09 -0700

    fix: remove unused code

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,18 +1,5 @@
 return {
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-      filetypes = {
-        markdown = true,
-        help = true,
-      },
-    },
-  },
-  {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "canary",
     dependencies = {
