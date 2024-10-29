f02c2a9f5a6048703ad51b8f917f23e7574efee4
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-10-29 11:24:59 -0700

    feat: add options for smarttab and shiftround

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,9 +6,12 @@ vim.opt.number = true -- Show line numbers by default
 vim.opt.relativenumber = true -- Enable relative line numbers, for help with jumping.
 vim.opt.cursorline = true -- Show which line your cursor is on
 vim.opt.scrolloff = 7 -- Minimal number of screen lines to keep above and below the cursor.
+
 vim.opt.expandtab = false -- Use tabs instead of spaces
 vim.opt.shiftwidth = 4 -- Size of an indent
 vim.opt.tabstop = 4 -- Number of spaces tabs count for
+vim.opt.smarttab = true -- Use shiftwidths at left margin, tabstops everywhere else
+vim.opt.shiftround = true -- Round indent to multiple of shiftwidth
 
 vim.opt.conceallevel = 0 -- Set conceal level to 0
 
