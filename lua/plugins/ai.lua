b7144eaa24d4c00a081af10c73a4d2601e430605
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-11-17 15:00:08 -0800

    feat: add olimorris/codecompanion.nvim

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -6,9 +6,21 @@ return {
       { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
       { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
     },
+    build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
-      model = "gpt-4o",
       auto_insert_mode = false,
     },
+  },
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "hrsh7th/nvim-cmp", -- Optional: For using slash commands and variables in the chat buffer
+      "nvim-telescope/telescope.nvim", -- Optional: For using slash commands
+      { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } }, -- Optional: For prettier markdown rendering
+      { "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves `vim.ui.select`
+    },
+    config = true,
   },
 }
